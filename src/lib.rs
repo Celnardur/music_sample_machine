0a3b4b3160504c6ef880ab3c22ae595dd74e10e9
Celnardur <celnardur@protonmail.com>
2021-05-06 18:02:21 -0900

    Added a basic error class

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,37 @@ pub mod sample;
 pub mod effect;
 
 
+#[derive(Debug, PartialEq)]
+pub struct Error {
+    message: String,
+}
+
+impl Error {
+    pub fn new_box(message: &str) -> Box<Error> {
+        Box::new(Error {
+            message: String::from(message),
+        })
+    }
+
+    pub fn new(message: &str) -> Error {
+        Error {
+            message: String::from(message),
+        }
+    }
+
+    pub fn message(&self) -> &str {
+        &self.message
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+}
